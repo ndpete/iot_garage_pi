679afd27461a09461908cbf0ad5aadf0cbc122a0
Nathan Peterson <ndpete@gmail.com>
2017-12-11 16:26:09 -0700

    testing

--- garage.py
+++ garage.py
@@ -2,6 +2,11 @@
 import time
 import RPi.GPIO as GPIO
 
+
+def sensor_callback(pin):
+    return GPIO.input(pin)
+
+
 # GPIO SETUP
 relay_pin = 27
 sensor_pin = 17
@@ -10,10 +15,11 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay_pin, GPIO.OUT)
 GPIO.setup(sensor_pin, GPIO.IN)
 GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+sensor_status = sensor_callback(sensor_pin)
 
 
 def check_sensor():
-    if GPIO.input(relay_pin):
+    if sensor_status:
         return "closed"
     else:
         return "open"
@@ -23,3 +29,6 @@ def toggle_door():
     GPIO.output(relay_pin, GPIO.HIGH)
     time.sleep(1)
     GPIO.output(relay_pin, GPIO.LOW)
+
+
+GPIO.add_event_detect(sensor_pin, GPIO.BOTH, callback=sensor_callback)
