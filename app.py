35806d185048d91109bf512de7fd8f89af9bf6f0
Nathan Peterson <ndpete@gmail.com>
2017-12-11 16:17:43 -0700

    testing functions

---
+++ app.py
@@ -0,0 +1,3 @@
+import garage
+
+garage.check_sensor()
--- garage.py
+++ garage.py
@@ -2,7 +2,7 @@
 import time
 import RPi.GPIO as GPIO
 
-#GPIO SETUP
+# GPIO SETUP
 relay_pin = 27
 sensor_pin = 17
 GPIO.setmode(GPIO.BCM)
@@ -17,27 +17,8 @@ def check_sensor():
     else:
         return "open"
 
+
 def toggle_door():
     GPIO.output(relay_pin, GPIO.HIGH)
     time.sleep(1)
     GPIO.output(relay_pin, GPIO.LOW)
-
-# High = On
-# GPIO.output(relay_pin, GPIO.HIGH)
-# # Low = Off
-# GPIO.output(relay_pin, GPIO.LOW)
-
-# Wired as NC
-# # COM -> BCM17, NC -> GROUND
-# GPIO.input(sensor_pin)
-# # 1 When closed
-# GPIO.input(sensor_pin)
-# 0 When Opened
-
-
-def sensor_callback(event):
-    print("sensor changed to " + str(GPIO.input(sensor_pin)))
-    print(event)
-
-
-GPIO.add_event_detect(sensor_pin, GPIO.BOTH, callback=sensor_callback)
