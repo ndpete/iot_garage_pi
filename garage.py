28819f072ea23a7d90812518c58548e285106ef9
Nathan Peterson <ndpete@gmail.com>
2017-12-11 21:04:14 -0700

    setup test

--- garage.py
+++ garage.py
@@ -3,32 +3,28 @@ import time
 import RPi.GPIO as GPIO
 
 
-def sensor_callback(pin):
-    return GPIO.input(pin)
-
-
 # GPIO SETUP
 relay_pin = 27
 sensor_pin = 17
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay_pin, GPIO.OUT)
-GPIO.setup(sensor_pin, GPIO.IN)
+GPIO.output(relay_pin, GPIO.LOW) # Set to Off might neet to check and set at boot if failed while open?
 GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-sensor_status = sensor_callback(sensor_pin)
 
 
-def check_sensor():
-    if sensor_status:
+def check_sensor(status):
+    if status:
         return "closed"
     else:
         return "open"
 
 
-def toggle_door():
-    GPIO.output(relay_pin, GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(relay_pin, GPIO.LOW)
-
-
-GPIO.add_event_detect(sensor_pin, GPIO.BOTH, callback=sensor_callback)
+print("Testing Setup....")
+print("Current State = {} Which is {}".format(GPIO.input(sensor_pin), check_sensor(GPIO.input(sensor_pin))))
+print("Triggering Relay")
+GPIO.output(relay_pin, GPIO.HIGH)
+print("Sleeping 2")
+time.sleep(1)
+print("Closing relay")
+GPIO.output(relay_pin, GPIO.LOW)
